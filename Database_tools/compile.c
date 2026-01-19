a630d5543c0fcd20125c2d0ddb24bc0898004094
Mizzo02 <izzo.matthew02@gmail.com>
2026-01-18 18:02:02 -0600

    compile program in progress

--- Database_tools/compile.c
+++ Database_tools/compile.c
@@ -1,21 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include "defs.h"
 
 #define OUTPUT_FILE "input.csv"
-#define NAME_MAX 15
-
-typedef struct item
-{
-	char name[25];
-	int count;
-	char owner[NAME_MAX];
-	struct item* next;
-} item_t;
 
 char user[NAME_MAX]; // global for direct access in functions
 
 int main()
 {
+	int i;
+	char check;
+	char trash = 'a'; // initalizing to anything other than '\n' works
+
+	item_t list_head;
+
 	printf( "Enter your username: " );
+
+	// for loop assigns the next character in the buffer to the array
+    for ( i = 0; (check = getchar()) != '\n' && i < NAME_MAX; i++ )
+    {
+        user[i] = check;
+    }
+
+    // adds a null terminator
+    user[i] = '\0';
+
+    // clears buffer of any extra characters
+    if ( i == NAME_MAX && check != '\n' )
+    {
+        while ( (trash = getchar()) != '\n' );
+    }
 }
